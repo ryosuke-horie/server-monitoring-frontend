a93601cc6a06f0b790ac2f6a9e1f32ece754ef0f
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-14 15:23:49 +0900

    いったん動くコードを用意

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@
 
 import { useState } from 'react';
 
+// TODO: 画面上部の日付と同期させた監視対象日のデータをPOSTする。（バックエンド実行後）
+// TODO: anyの部分を型定義する
+
 export default function MyForm() {
   const currentDate = new Date();
   const formattedDate = `${currentDate.getFullYear()}/${String(currentDate.getMonth() + 1).padStart(2, '0')}/${String(currentDate.getDate()).padStart(2, '0')}`;
@@ -27,6 +30,9 @@ export default function MyForm() {
   const [checkboxes, setCheckboxes] = useState({
     example1: { visual: false, zabbix: false, backup: false },
     example2: { visual: false, zabbix: false, backup: false },
+    example3: { visual: false, zabbix: false, backup: false },
+    example4: { visual: false, zabbix: false, backup: false },
+    example5: { visual: false, zabbix: false, backup: false },
   });
 
   type CheckboxType = {
@@ -45,6 +51,69 @@ export default function MyForm() {
     setCheckboxes(updatedCheckboxes);
   };
 
+  // textarea の入力値を管理する state
+  const [reportItems, setReportItems] = useState({
+    example1: '',
+    example2: '',
+    example3: '',
+    example4: '',
+    example5: '',
+  });
+
+  const submitData = async () => {
+    const currentDate = new Date();
+    currentDate.setTime(currentDate.getTime() + 9 * 60 * 60 * 1000);
+    const isoString = currentDate.toISOString();
+
+    const rows = [
+      { target_name: "パチンコビスタ", target_ip: "192.168.20.32", is_working: String(checkboxes.example1.visual), is_backup_completed: String(checkboxes.example1.backup), is_not_alert: String(checkboxes.example1.zabbix), created_at: isoString, updated_at: isoString },
+      { target_name: "券売機", target_ip: "192.168.20.32", is_working: String(checkboxes.example2.visual), is_backup_completed: String(checkboxes.example2.backup), is_not_alert: String(checkboxes.example2.zabbix), created_at: isoString, updated_at: isoString },
+      { target_name: "エフ・エス", target_ip: "192.168.20.32", is_working: String(checkboxes.example3.visual), is_backup_completed: String(checkboxes.example3.backup), is_not_alert: String(checkboxes.example3.zabbix), created_at: isoString, updated_at: isoString },
+      { target_name: "グループセッション", target_ip: "192.168.20.32", is_working: String(checkboxes.example4.visual), is_backup_completed: String(checkboxes.example4.backup), is_not_alert: String(checkboxes.example4.zabbix), created_at: isoString, updated_at: isoString },
+      { target_name: "券売機プロ", target_ip: "192.168.20.32", is_working: String(checkboxes.example5.visual), is_backup_completed: String(checkboxes.example5.backup), is_not_alert: String(checkboxes.example5.zabbix), created_at: isoString, updated_at: isoString },
+    ];
+
+    // ローカルストレージからaccess_tokenを取得
+    const accessToken = localStorage.getItem('accessToken');
+    if (!accessToken) {
+      console.error("No access token found in local storage");
+      return;
+    }
+
+    // x-www-form-urlencoded形式でエンコードする関数
+    const formUrlEncode = (obj) => Object.keys(obj)
+      .map(k => `${encodeURIComponent(k)}=${encodeURIComponent(obj[k])}`)
+      .join('&');
+
+    // 各行のデータを1件ずつPOSTする
+    for (const payload of rows) {
+      console.log(payload);
+      try {
+        const response = await fetch('http://54.199.212.225:3000/monitoring', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': `Bearer ${accessToken}`
+          },
+          body: formUrlEncode(payload) // bodyをエンコードする
+        });
+
+        if (response.status === 201) { // 成功時のステータスコードを201としてチェック
+          const data = await response.json();
+          console.log(data);
+        } else {
+          const errorData = await response.json();
+          console.error(`Error: ${response.statusText}`);
+          console.error(errorData);
+        }
+      } catch (error) {
+        console.error("There was an error submitting the data", error);
+      }
+    };
+
+  };
+
+
   return (
     <div>
       <div className="header">
@@ -52,7 +121,7 @@ export default function MyForm() {
         <span className="date-header">{date}</span>
         <button onClick={incrementDate}>＞</button>
         <button onClick={selectAll}>全選択</button>
-        <button>送信</button>
+        <button onClick={submitData}>送信</button>
       </div>
       <table>
         <thead>
@@ -70,35 +139,35 @@ export default function MyForm() {
             <td><input type="checkbox" checked={checkboxes.example1.visual} onChange={(e) => setCheckboxes(prev => ({ ...prev, example1: { ...prev.example1, visual: e.target.checked } }))} /></td>
             <td><input type="checkbox" checked={checkboxes.example1.zabbix} onChange={(e) => setCheckboxes(prev => ({ ...prev, example1: { ...prev.example1, zabbix: e.target.checked } }))} /></td>
             <td><input type="checkbox" checked={checkboxes.example1.backup} onChange={(e) => setCheckboxes(prev => ({ ...prev, example1: { ...prev.example1, backup: e.target.checked } }))} /></td>
-            <td><textarea /></td>
+            <td><textarea value={reportItems.example1} onChange={(e) => setReportItems(prev => ({ ...prev, example1: e.target.value }))} /></td>
           </tr>
           <tr>
             <td>券売機</td>
             <td><input type="checkbox" checked={checkboxes.example2.visual} onChange={(e) => setCheckboxes(prev => ({ ...prev, example2: { ...prev.example2, visual: e.target.checked } }))} /></td>
             <td><input type="checkbox" checked={checkboxes.example2.zabbix} onChange={(e) => setCheckboxes(prev => ({ ...prev, example2: { ...prev.example2, zabbix: e.target.checked } }))} /></td>
             <td><input type="checkbox" checked={checkboxes.example2.backup} onChange={(e) => setCheckboxes(prev => ({ ...prev, example2: { ...prev.example2, backup: e.target.checked } }))} /></td>
-            <td><textarea /></td>
+            <td><textarea value={reportItems.example2} onChange={(e) => setReportItems(prev => ({ ...prev, example2: e.target.value }))} /></td>
           </tr>
           <tr>
             <td>エフ・エス</td>
-            <td><input type="checkbox" checked={checkboxes.example2.visual} onChange={(e) => setCheckboxes(prev => ({ ...prev, example2: { ...prev.example2, visual: e.target.checked } }))} /></td>
-            <td><input type="checkbox" checked={checkboxes.example2.zabbix} onChange={(e) => setCheckboxes(prev => ({ ...prev, example2: { ...prev.example2, zabbix: e.target.checked } }))} /></td>
-            <td><input type="checkbox" checked={checkboxes.example2.backup} onChange={(e) => setCheckboxes(prev => ({ ...prev, example2: { ...prev.example2, backup: e.target.checked } }))} /></td>
-            <td><textarea /></td>
+            <td><input type="checkbox" checked={checkboxes.example3.visual} onChange={(e) => setCheckboxes(prev => ({ ...prev, example3: { ...prev.example3, visual: e.target.checked } }))} /></td>
+            <td><input type="checkbox" checked={checkboxes.example3.zabbix} onChange={(e) => setCheckboxes(prev => ({ ...prev, example3: { ...prev.example3, zabbix: e.target.checked } }))} /></td>
+            <td><input type="checkbox" checked={checkboxes.example3.backup} onChange={(e) => setCheckboxes(prev => ({ ...prev, example3: { ...prev.example3, backup: e.target.checked } }))} /></td>
+            <td><textarea value={reportItems.example3} onChange={(e) => setReportItems(prev => ({ ...prev, example3: e.target.value }))} /></td>
           </tr>
           <tr>
             <td>グループセッション</td>
-            <td><input type="checkbox" checked={checkboxes.example2.visual} onChange={(e) => setCheckboxes(prev => ({ ...prev, example2: { ...prev.example2, visual: e.target.checked } }))} /></td>
-            <td><input type="checkbox" checked={checkboxes.example2.zabbix} onChange={(e) => setCheckboxes(prev => ({ ...prev, example2: { ...prev.example2, zabbix: e.target.checked } }))} /></td>
-            <td><input type="checkbox" checked={checkboxes.example2.backup} onChange={(e) => setCheckboxes(prev => ({ ...prev, example2: { ...prev.example2, backup: e.target.checked } }))} /></td>
-            <td><textarea /></td>
+            <td><input type="checkbox" checked={checkboxes.example4.visual} onChange={(e) => setCheckboxes(prev => ({ ...prev, example4: { ...prev.example4, visual: e.target.checked } }))} /></td>
+            <td><input type="checkbox" checked={checkboxes.example4.zabbix} onChange={(e) => setCheckboxes(prev => ({ ...prev, example4: { ...prev.example4, zabbix: e.target.checked } }))} /></td>
+            <td><input type="checkbox" checked={checkboxes.example4.backup} onChange={(e) => setCheckboxes(prev => ({ ...prev, example4: { ...prev.example4, backup: e.target.checked } }))} /></td>
+            <td><textarea value={reportItems.example4} onChange={(e) => setReportItems(prev => ({ ...prev, example4: e.target.value }))} /></td>
           </tr>
           <tr>
             <td>券売機プロ</td>
-            <td><input type="checkbox" checked={checkboxes.example2.visual} onChange={(e) => setCheckboxes(prev => ({ ...prev, example2: { ...prev.example2, visual: e.target.checked } }))} /></td>
-            <td><input type="checkbox" checked={checkboxes.example2.zabbix} onChange={(e) => setCheckboxes(prev => ({ ...prev, example2: { ...prev.example2, zabbix: e.target.checked } }))} /></td>
-            <td><input type="checkbox" checked={checkboxes.example2.backup} onChange={(e) => setCheckboxes(prev => ({ ...prev, example2: { ...prev.example2, backup: e.target.checked } }))} /></td>
-            <td><textarea /></td>
+            <td><input type="checkbox" checked={checkboxes.example5.visual} onChange={(e) => setCheckboxes(prev => ({ ...prev, example5: { ...prev.example5, visual: e.target.checked } }))} /></td>
+            <td><input type="checkbox" checked={checkboxes.example5.zabbix} onChange={(e) => setCheckboxes(prev => ({ ...prev, example5: { ...prev.example5, zabbix: e.target.checked } }))} /></td>
+            <td><input type="checkbox" checked={checkboxes.example5.backup} onChange={(e) => setCheckboxes(prev => ({ ...prev, example5: { ...prev.example5, backup: e.target.checked } }))} /></td>
+            <td><textarea value={reportItems.example5} onChange={(e) => setReportItems(prev => ({ ...prev, example5: e.target.value }))} /></td>
           </tr>
         </tbody>
       </table>
