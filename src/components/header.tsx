0e53a1c79e0189ff6d722982cde881f5e1d03b98
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-15 17:09:09 +0900

    文字や不要アイコンの削除

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,8 +4,6 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import Link from 'next/link';
 
 export default function Header() {
@@ -13,17 +11,8 @@ export default function Header() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            サーバー監視効率化ツール
           </Typography>
           <Button color="inherit"><Link href="/signin">SignIn</Link></Button>
           <Button color="inherit"><Link href="/signup">SignUp</Link></Button>
