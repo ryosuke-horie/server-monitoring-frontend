a48353a28d1c74b22b26a7ce5ecdf418de8ca234
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-23 22:59:00 +0900

    リンクの色を変更してUX改善

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,6 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import Link from 'next/link';
 
 export default function Header() {
@@ -15,16 +14,32 @@ export default function Header() {
             サーバー監視効率化ツール
           </Typography>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Link href="/">サーバー監視記録ページ</Link>
+            <Link href="/" passHref>
+              <button style={{ background: 'none', border: 'none', color: 'white', cursor: 'pointer' }}>
+                サーバー監視記録ページ
+              </button>
+            </Link>
           </Typography>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Link href="/report_list">月別監視記録レポート一覧</Link>
+            <Link href="/report_list" passHref>
+              <button style={{ background: 'none', border: 'none', color: 'white', cursor: 'pointer' }}>
+                月別監視記録レポート一覧
+              </button>
+            </Link>
           </Typography>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Link href="/signin">SignIn</Link>
+            <Link href="/signin" passHref>
+              <button style={{ background: 'none', border: 'none', color: 'white', cursor: 'pointer' }}>
+                SignIn
+              </button>
+            </Link>
           </Typography>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Link href="/signup">signup</Link>
+            <Link href="/signup" passHref>
+              <button style={{ background: 'none', border: 'none', color: 'white', cursor: 'pointer' }}>
+                SignUp
+              </button>
+            </Link>
           </Typography>
         </Toolbar>
       </AppBar>
