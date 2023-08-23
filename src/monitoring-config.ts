f4f06f5e421d5f47fbd0ea39a1945c84d9576f4c
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-23 22:35:49 +0900

    残数照会を追加

--- src/monitoring-config.ts
+++ src/monitoring-config.ts
@@ -1,8 +1,9 @@
 // 監視対象サービスのリスト。
 export const MONITORING_TARGETS = [
-  { key: "example1", name: "パチンコビスタ", ip: "192.168.20.32" },
-  { key: "example2", name: "券売機", ip: "192.168.20.32" },
-  { key: "example3", name: "エフエス", ip: "192.168.20.32" },
-  { key: "example4", name: "グループセッション", ip: "192.168.20.32" },
-  { key: "example5", name: "券売機プロ", ip: "192.168.20.32" },
+  { key: "example1", name: "パチンコビスタ", ip: "www.pachinkovista.com" },
+  { key: "example2", name: "券売機", ip: "www.operal.jp" },
+  { key: "example3", name: "エフエス", ip: "www.fscorp.jp" },
+  { key: "example4", name: "グループセッション", ip: "www.j-net-gs.com" },
+  { key: "example5", name: "券売機プロ", ip: "www.kenbaiki-pro.jp" },
+  { key: "example6", name: "残数照会", ip: "www.chodama-zansuu.jp"},
 ];
