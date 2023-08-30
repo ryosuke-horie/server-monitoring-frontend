1be6d6f91ad04173002b508906f517c6a71a750a
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-31 00:19:05 +0900

    :bug: WhitelistのIPを修正

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-const IP_WHITELIST = ['192.168.3.6']; // あなたのIPホワイトリスト
+const IP_WHITELIST = ['172.31.208.1']; // あなたのIPホワイトリスト
 
 export default async function middleware(req: NextRequest) {
   const clientIP = req.headers['x-forwarded-for'] || req.headers['x-real-ip'];
