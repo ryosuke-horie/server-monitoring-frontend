b456fb9e08f72090c6e2e447d63aac25d2bc47fe
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-31 00:13:08 +0900

    :bug: リダイレクト先のドメインを変更

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ export default async function middleware(req: NextRequest) {
   const clientIP = req.headers['x-forwarded-for'] || req.headers['x-real-ip'];
 
   if (!IP_WHITELIST.includes(clientIP)) {
-    return NextResponse.redirect('http://localhost:3000/access-denied'); // アクセス拒否のページにリダイレクト
+    return NextResponse.redirect('https://server-monitoring-prototype.vercel.app/access-denied'); // アクセス拒否のページにリダイレクト
   }
 
   return NextResponse.next();
